628877e8aceab4bf0dade252d435fec7c4aaa3c5
Sid Raval <sidsraval@gmail.com>
2014-01-14 19:37:23 -0500

    Adds horribly coded, untested version.

--- js/mondrian.js
+++ js/mondrian.js
@@ -7,6 +7,12 @@ function toggleColor($el, newColor) {
   $el.addClass(newColor);
 }
 
+function createTimer($box, timeout) {
+  setTimeout(function() {
+    toggleColor($box, ["red", "blue", "yellow", "gray"].randomElement())
+  }, timeout);
+}
+
 $(function() {
   for(var i = 0; i < 30; i++) {
     $(".columns").append("<div class='column-box' data-id='" + i + "'>" + (parseInt(i) + 1) + "</div>")
@@ -15,6 +21,26 @@ $(function() {
       $(".canvas").append("<div class='box' data-id='[" + i + "," + j + "]'></div>");
     }
   }
+
+  $(".box").hover(function(event) {
+    var $e = $(event.currentTarget);
+
+    var pos = [$e.data("id")[0], $e.data("id")[1]];
+
+    // console.log("[data-id='" + pos + "']");
+
+    $(".rows [data-id='" + pos[0] + "']").addClass('pink');
+    $(".columns [data-id='" + pos[1] + "']").addClass('pink');
+  }, function(event) {
+    var $e = $(event.currentTarget);
+
+    var pos = [$e.data("id")[0], $e.data("id")[1]];
+
+    // console.log("[data-id='" + pos + "']");
+
+    $(".rows [data-id='" + pos[0] + "']").removeClass('pink');
+    $(".columns [data-id='" + pos[1] + "']").removeClass('pink');
+  });
 
   $(".box").on("click", function(event) {
     $e = $(event.currentTarget);
@@ -32,12 +58,21 @@ $(function() {
     var column = $e.data("id");
 
     console.log(column);
+    var delay = 0;
 
     $(".box").each(function(index, box) {
+      var random = (Math.floor(Math.random() * 30));
+
       $box = $(box);
 
       if ($box.data("id")[0] == column) {
-        toggleColor($box, ["red", "blue", "yellow", "gray"].randomElement())
+        delay += 30;
+        createTimer($box, delay/4.0);
+
+        if (random == 13) {
+          console.log("FOUND 13");
+          return false;
+        }
       }
     })
   })
@@ -46,14 +81,29 @@ $(function() {
     $e = $(event.currentTarget);
     var column = $e.data("id");
 
-    console.log(column);
+    $(".box").each(function(index, box) {
+      $box = $(box)
+
+      if ($box.data("id")[1] == column) {
+        $box.addClass("white");
+      }
+
+    })
 
     $(".box").each(function(index, box) {
+      var random = (Math.floor(Math.random() * 30));
+
       $box = $(box);
 
       if ($box.data("id")[1] == column) {
-        toggleColor($box, ["red", "blue", "yellow", "gray"].randomElement())
+        createTimer($box, index/4.0);
+        if (random == 13) {
+          console.log("FOUND 13");
+          return false;
+        }
       }
     })
   })
+
+
 })
